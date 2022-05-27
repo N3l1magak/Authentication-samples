4d4afd4badd8e9ccd3933984478ebdabc54debe7
N3l1magak <renhouzhang2021@gmail.com>
2022-05-27 16:36:27 -0400

    Added post actions for both register and login page

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 const express =  require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("home");
@@ -22,9 +32,32 @@ app.get("/login", function(req, res){
   res.render("login");
 });
 
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if(!err) {
+      console.log("Successfully saved new user.");
+      res.render("secrets");
+    }
+  });
+});
 
-
-
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, result){
+    if(!err){
+      if(result) {
+        if(result.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 
 
